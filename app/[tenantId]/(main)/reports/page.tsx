08b6cb94b210f319c582a46d1923ad02fc7210c0
Emre SUALP <esualp95@gmail.com>
2025-01-02 12:11:51 +0300

    şlkfsd

--- app/[tenantId]/(main)/reports/page.tsx
+++ app/[tenantId]/(main)/reports/page.tsx
@@ -1,12 +1,11 @@
 "use client"
-import ReportTable from "./report-table"
 import ReportTableBigQuery from "./report-table-bigquery"
 import { ReportPageProps } from "./types"
 
 export const ReportPage = ({report, reportGroup}: ReportPageProps) => {
     return (
         <div>
-            <ReportTable report={report} reportGroup={reportGroup}/>
+            <ReportTableBigQuery report={report} reportGroup={reportGroup}/>
         </div>
     )
 }
