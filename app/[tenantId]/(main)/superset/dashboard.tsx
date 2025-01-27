19ef8d4c76bdbfbf161c425c8a2b8b98b0708272
Emre SUALP <esualp95@gmail.com>
2025-01-27 18:44:03 +0300

    şlkfsd

--- app/[tenantId]/(main)/superset/dashboard.tsx
+++ app/[tenantId]/(main)/superset/dashboard.tsx
@@ -7,8 +7,8 @@ import { useDatabase } from '@/hooks/use-database';
 import axios from "@/lib/axios";
 
 
-const SUPERSET_BASE_URL = `${typeof window !== 'undefined' ? window.location.protocol + '//' + process.env.NEXT_PUBLIC_SUPERSET_HOST : ''}:${process.env.NEXT_PUBLIC_SUPERSET_PORT}`;
-const SUPERSET_BASE_URL2 = `${ 'http://' + process.env.NEXT_PUBLIC_SUPERSET_HOST }:${process.env.NEXT_PUBLIC_SUPERSET_PORT}`;
+const SUPERSET_BASE_URL2 = `${typeof window !== 'undefined' ? window.location.protocol + '//' + process.env.NEXT_PUBLIC_SUPERSET_HOST : ''}:${process.env.NEXT_PUBLIC_SUPERSET_PORT}`;
+const SUPERSET_BASE_URL = `${ 'http://' + process.env.NEXT_PUBLIC_SUPERSET_HOST }:${process.env.NEXT_PUBLIC_SUPERSET_PORT}`;
 
 const tokenCache: Record<string, TokenCache> = {};
 const TOKEN_EXPIRY_MINUTES = 50;
