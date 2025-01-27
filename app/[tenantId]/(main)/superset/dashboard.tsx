a7b934e711f9ebf5e25e998298cf2d302ccdcb7b
Emre SUALP <esualp95@gmail.com>
2025-01-27 18:43:45 +0300

    şlkfsd

--- app/[tenantId]/(main)/superset/dashboard.tsx
+++ app/[tenantId]/(main)/superset/dashboard.tsx
@@ -7,7 +7,9 @@ import { useDatabase } from '@/hooks/use-database';
 import axios from "@/lib/axios";
 
 
-const SUPERSET_BASE_URL = `${ 'http://' + process.env.NEXT_PUBLIC_SUPERSET_HOST }:${process.env.NEXT_PUBLIC_SUPERSET_PORT}`;
+const SUPERSET_BASE_URL = `${typeof window !== 'undefined' ? window.location.protocol + '//' + process.env.NEXT_PUBLIC_SUPERSET_HOST : ''}:${process.env.NEXT_PUBLIC_SUPERSET_PORT}`;
+const SUPERSET_BASE_URL2 = `${ 'http://' + process.env.NEXT_PUBLIC_SUPERSET_HOST }:${process.env.NEXT_PUBLIC_SUPERSET_PORT}`;
+
 const tokenCache: Record<string, TokenCache> = {};
 const TOKEN_EXPIRY_MINUTES = 50;
 
@@ -61,6 +63,8 @@ export default function SupersetDashboardComponent({ dashboardId, standalone, ex
     const tenantId = params.tenantId as string;
     const { database } = useDatabase(tenantId);
 
+    console.log("SUPERSET_BASE_URL", SUPERSET_BASE_URL)
+    console.log("SUPERSET_BASE_URL2", SUPERSET_BASE_URL2)
     if (!SUPERSET_BASE_URL) {
         return <div>Hata: Ayarlar Alınamadı</div>;
     }
